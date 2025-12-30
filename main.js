80577bf3658f790843f852385dc0a5be87a4fc0e
LaryssaPam <laryssapam6@gmail.com>
2025-12-30 15:05:23 -0400

    push

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const contentDiv = document.getElementsByClassName("content");
+
+console.log("contentDiv", contentDiv);
+
+const laryssa = document.getElementById('laryssa');
+console.log("laryssa",laryssa);
